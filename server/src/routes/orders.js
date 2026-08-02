2de2dfc0c6280f9ca15dcd798ad48cda53621a5f
wigglebop25 <jasperxtaguiam@gmail.com>
2026-08-02 19:57:31 +0800

    feat: add order routes

---
+++ server/src/routes/orders.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const orderRepository = require('../repositories/orderRepository');
+const productRepository = require('../repositories/productRepository');
+const OrderService = require('../services/orderService');
+const OrderController = require('../controllers/orderController');
+
+const orderService = new OrderService(orderRepository, productRepository);
+const orderController = new OrderController(orderService);
+
+router.get('/', (req, res, next) => orderController.getAll(req, res, next));
+router.get('/:id', (req, res, next) => orderController.getById(req, res, next));
+router.post('/', (req, res, next) => orderController.create(req, res, next));
+router.put('/:id/status', (req, res, next) => orderController.updateStatus(req, res, next));
+
+module.exports = router;
